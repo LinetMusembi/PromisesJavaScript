62702de1b1de3c5cebf52f54553862f2acb083d3
linetmusembi <musembilinetmwongeli@gmail.com>
2023-05-11 15:49:10 +0300

    first commit

--- promise.js
+++ promise.js
@@ -1,23 +1,30 @@
 // Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
 // The function should log the message to the console after the specified delay time.
-const myInterest=()=>{
-    console.log("I love swimming")
+
+const myInterest = ()=>{
+    console.log("I enjoy learning javascript");
 }
-setTimeout(myInterest,2000);
-
-
-
-
-
-
+setTimeout(myInterest,1000)
 
 // You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given 
 // a user ID.Write an asynchronous function that fetches and logs the data for each user ID one by one, 
 // in sequence.
+const userIds = [2,3,4,5,6,7,7]
 
-function getUserData() {
-    
+async function getUserData(){
+    for (const id of userIds){
+        const userData = await userIds(id);
+        console.log(userData);
+    }
 }
+
+getUserData()
+
+
+
+
+
+
 
 
 
